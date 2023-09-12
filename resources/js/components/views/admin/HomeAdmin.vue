86ca5a913db78cc54296b23c8ecb7f6de5e768d3
Aldo Bressan <aldo@myvos.med.br>
2023-09-11 22:39:39 -0300

    feat: Teste de Phpstorm

--- resources/js/components/views/admin/HomeAdmin.vue
+++ resources/js/components/views/admin/HomeAdmin.vue
@@ -1,6 +1,7 @@
 <template>
-  <v-container fluid>
+  <v-container fluid="2">
     <v-row> Bem vindo à área administrativa do seu site! </v-row>
+      <v-card>Teste</v-card>
   </v-container>
 </template>
 
